a1ac376a86f9751c6a7484a841fd68b1e1cc9da5
luckychip-io <luckychipdev@protonmail.com>
2021-07-27 13:10:20 +0800

    modify package to run expectRevert

--- test/DiceBNB.test.js
+++ test/DiceBNB.test.js
@@ -2,7 +2,6 @@ const { expectRevert, time } = require('@openzeppelin/test-helpers');
 const { AddressZero } = require("@ethersproject/constants")
 const { assert } = require('chai');
 const ethers = require('ethers');
-const JSBI           = require('jsbi')
 const DiceToken = artifacts.require('DiceToken');
 const WBNB = artifacts.require('libs/WBNB');
 const DiceBNB = artifacts.require('DiceBNB');
@@ -27,17 +26,39 @@ contract('DiceBNB', ([alice, bob, carol, dev, refFeeAddr, operator, minter]) => 
 		await this.dice.setAdmin(dev, {from: minter});
 		await this.dice.setOperator(operator, {from: dev});
 
-		await this.dice.deposit({from: alice, value: 1});
-		await this.dice.deposit({from: bob, value: 2});
+		await this.dice.deposit({from: alice, value: 10});
+		await this.dice.deposit({from: bob, value: 20});
 
-		assert(await this.dice.bankerAmount().toString(), '2');
-		assert(await this.dice.canWithdrawToken(alice).toString(), '1');
+		assert.equal((await this.dice.bankerAmount()).toString(), '30');
+		assert.equal((await this.dice.canWithdrawToken(alice)).toString(), '10');
 
 		let randomNumber = ethers.utils.hexlify(ethers.utils.randomBytes(32));
-		console.log(randomNumber);
 		let bankHash = ethers.utils.keccak256(randomNumber);
-		console.log(bankHash);
+		console.log(randomNumber, bankHash);
+		await expectRevert(this.dice.endBankerTime(1, bankHash, {from: minter}), 'operator: wut?');
 		await this.dice.endBankerTime(1, bankHash, {from: operator});
+		
+		await this.dice.betNumber([false,false,true,false,false,false], {from:alice, value:2});
+		await this.dice.betNumber([true,true,true,true,true,true], {from:bob, value:7});
+
+		let round = await this.dice.rounds(1);
+		for(var i = 0; i < 13; i ++){
+			console.log(`${round[i]}`);
+		}
+
+		lockBlock = round[1];
+		console.log(`Current block: ${(await time.latestBlock())},${lockBlock}`);
+		await expectRevert(this.dice.claim(1, {from: alice}), 'Round has not locked');
+		await time.advanceBlockTo(lockBlock);
+		let newRandomNumber = ethers.utils.hexlify(ethers.utils.randomBytes(32));
+		let newBankHash = ethers.utils.keccak256(newRandomNumber);
+		console.log(newRandomNumber, newBankHash);
+		await this.dice.executeRound(1, newBankHash, {from: operator});
+		await this.dice.sendSecret(1, randomNumber, {from: operator});
+
+		round = await this.dice.rounds(1);
+		console.log(`finalNumber,${round[11]}`);
+		
 
     });
 });
