8e6d9d489475e03f4fdf93cdcb257943dbcb80d7
luckychip-io <luckychipdev@protonmail.com>
2021-09-06 18:25:37 +0800

    modify migration script

--- migrations/1_LC.js
+++ migrations/1_LC.js
@@ -9,17 +9,22 @@ module.exports = async function(deployer) {
 
   await deployer.deploy(
 	Dice,
-	"0x8909BbBe374cF5158BEB18b1A8D5e93BC3e2C30F",
-	"0x8909BbBe374cF5158BEB18b1A8D5e93BC3e2C30F",
+	"0xD263fA5F95F7F2Cbd0218BA83E9baFD29971A3D6",
+	"0xD263fA5F95F7F2Cbd0218BA83E9baFD29971A3D6",
 	instanceToken.address,
-	"0xCc82850F42281E71f75ECc675ABdCc92891ec704",
+	"0x7aB09C37Dd457EDf843f1e27C415BF9cE3F11828",
 	0,
 	20,
 	500,
 	600,
     ethers.utils.parseEther('0.001'),
     ethers.utils.parseEther('0.001'),
-    ethers.utils.parseEther('300000')
+    ethers.utils.parseEther('300000000000')
 	);
+
+  let instanceDice = await Dice.deployed();
+  console.log(instanceDice.address);
+  await instanceToken.transferOwnership(instanceDice.address);
+  console.log('transferOwnership done');
 
 };
--- migrations/2_WBNB.js
+++ migrations/2_WBNB.js
@@ -10,10 +10,10 @@ module.exports = async function(deployer) {
   await deployer.deploy(
 	Dice,
 	"0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
-	"0x0bd5B0bF4dA8249ed9502eeD2ed318D17C843f89",
+	"0xD263fA5F95F7F2Cbd0218BA83E9baFD29971A3D6",
 	instanceToken.address,
-	"0xB0940bf20358011093ea4391b3f15981A3e53Ab0",
-	0,
+	"0x7aB09C37Dd457EDf843f1e27C415BF9cE3F11828",
+	1,
 	20,
 	500,
 	600,
@@ -22,4 +22,8 @@ module.exports = async function(deployer) {
     ethers.utils.parseEther('300000')
 	);
 
+  let instanceDice = await Dice.deployed();
+  console.log(instanceDice.address);
+  await instanceToken.transferOwnership(instanceDice.address);
+  console.log('transferOwnership done');
 };
