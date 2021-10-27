53ab7f28e0778e279b78830424f0dfe09b4a7770
lucky-jack <luckychipdev@protonmail.com>
2021-10-27 15:50:11 +0800

    modify Lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,20 +7,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./interfaces/IBEP20.sol";
 import "./interfaces/ILottery.sol";
+import "./interfaces/ILuckyPower.sol";
 import "./libraries/SafeBEP20.sol";
 
 contract Lottery is ILottery, Ownable, ReentrancyGuard{
     using SafeMath for uint256;
     using SafeBEP20 for IBEP20;
 
-    IBEP20 public lcToken;
     mapping(address => uint256) public balanceOf;
+    IBEP20 public lcToken;
+    ILuckyPower public luckyPower;
 
     event InjectFunds(address indexed src, address indexed dst, uint256 blockNumber, uint256 amount);
     event ClaimLottery(address indexed user, uint256 blockNumber, uint256 amount);
+    event SetLuckyPower(uint256 indexed block, address luckyPowerAddr);
 
-    constructor(address _lcTokenAddress) public {
-        lcToken = IBEP20(_lcTokenAddress);
+    constructor(address _lcTokenAddr) public {
+        lcToken = IBEP20(_lcTokenAddr);
     }
 
     modifier notContract() {
@@ -40,6 +43,9 @@ contract Lottery is ILottery, Ownable, ReentrancyGuard{
     function injectFunds(address dst, uint256 amount) public nonReentrant notContract {
         lcToken.safeTransferFrom(address(msg.sender), address(this), amount);
         balanceOf[dst] = balanceOf[dst].add(amount);
+        if(address(luckyPower) != address(0)){
+            luckyPower.updatePower(dst);
+        }
         emit InjectFunds(msg.sender, dst, block.number, amount); 
     }
 
@@ -48,11 +54,21 @@ contract Lottery is ILottery, Ownable, ReentrancyGuard{
         if(amount > 0) {
             balanceOf[msg.sender] = 0;
             lcToken.safeTransfer(msg.sender, amount);
+            if(address(luckyPower) != address(0)){
+                luckyPower.updatePower(msg.sender);
+            }
             emit ClaimLottery(msg.sender, block.number, amount); 
         }
     }
     
     function getLuckyPower(address user) external override view returns (uint256){
         return balanceOf[user];
+    }
+
+    // set the lucky power.
+    function setLuckyPower(address _luckyPower) external onlyOwner {
+        require(_luckyPower != address(0), "Zero");
+        luckyPower = ILuckyPower(_luckyPower);
+        emit SetLuckyPower(block.number, _luckyPower);
     }
 }
